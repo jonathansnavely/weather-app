70cde054e993c1c1dd3551b904e0f31d3770ce5f
Snavely, Jonathan <JSnavely@cvent.com>
2020-02-13 10:10:17 -0700

    Main screen solution

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import FiveDayForcast from './FiveDayForcast';
 
-class App extends React.Component {
-  componentDidMount() {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?zip=94040&units=imperial&appid=8df9894bcace71d40e078411678d7fe8')
-      .then((response) => {
-        return response.json();
-      })
-      .then((myJson) => {});
-  }
+const App = () => {
+  const [zipCode, setZipCode] = useState();
+  const [apiData, setApiData] = useState();
 
-  render() {
-    return (
-      <div className="d-flex flex-column justify-content-between border border-dark mt-3 ml-3" style={{ width: 400, height: 500 }}>
-        <div>
-          <h4 className="border-bottom border-dark p-3">Weather App</h4>
-          <FiveDayForcast />
-        </div>
-        <div className="d-flex flex-column align-items-center m-3">
-          <input className="form-control" type="text" placeholder="Zipcode" />
-          <button className="btn btn-outline-secondary mt-3" style={{ width: 200 }}>
-            Submit
-          </button>
-        </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      const zip = 85282;
+      const units = 'imperial';
+      const appid = '8df9894bcace71d40e078411678d7fe8';
+
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&units=${units}&appid=${appid}`);
+      const data = await response.json();
+      setApiData(data);
+      return data;
+    };
+
+    if (zipCode?.length === 5) {
+      fetchData();
+    }
+  }, [zipCode, setZipCode]);
+
+  const handleZipChange = e => {
+    const zipCodeInput = e.target.value;
+
+    if (String(zipCodeInput).length <= 5) {
+      setZipCode(zipCodeInput);
+    }
+  };
+
+  return (
+    <div className="d-flex flex-column justify-content-between border border-dark mt-3 ml-3" style={{ width: 400, height: 500 }}>
+      <div>
+        <h4 className="border-bottom border-dark p-3">Weather App</h4>
+        <FiveDayForcast data={apiData} />
       </div>
-    );
-  }
-}
+      <div className="d-flex flex-column align-items-center m-3">
+        <input className="form-control" type="number" placeholder="Zipcode" onChange={handleZipChange} value={zipCode} />
+        <button className="btn btn-outline-secondary mt-3" style={{ width: 200 }}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/FiveDayForcast.js
+++ src/FiveDayForcast.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import moment from 'moment';
 
-const Day = ({ onClick }) => {
+const Day = ({ onClick, forecast, dayName }) => {
   const defaultWeatherIcon = '10d';
+
+  if (!forecast || !forecast.length) {
+    return null;
+  }
+
+  const min = Math.min(...forecast.map(time => time.main.temp_min));
+  const max = Math.max(...forecast.map(time => time.main.temp_max));
 
   return (
     <div className="d-flex flex-column align-items-center" style={{ width: '20%' }}>
       <div>
-        Day
+        {dayName}
       </div>
       <img
         className="border"
@@ -16,20 +24,36 @@ const Day = ({ onClick }) => {
         onClick={onClick}
       />
       <div className="pt-3" style={{ fontSize: '0.5rem'}}>
-        {`25F / 45F`}
+        {`${min}F / ${max}F`}
       </div>
     </div>
   );
 };
 
-const FiveDayForecast = () => {
+const FiveDayForecast = ({ data }) => {
+  let daysToRender = {};
+
+  if (data) {
+    const weatherTimes = data.list;
+
+    daysToRender = weatherTimes.reduce((agg, item) => {
+      const day = moment(item.dt * 1000);
+      const dayPosition = day.format('dddd');
+
+      if (agg[dayPosition]) {
+        agg[dayPosition].push(item);
+      } else {
+        agg[dayPosition] = [item];
+      }
+
+      return agg;
+    }, {});
+  }
+
   return (
     <div className="d-flex justify-content-between">
-      <Day />
-      <Day />
-      <Day />
-      <Day />
-      <Day />
+      {Object.keys(daysToRender).slice(0, 5).map((dayName, i) =>
+        <Day key={i} forecast={daysToRender[dayName]} dayName={dayName} onClick={() => alert('todo')} />)}
     </div>
   )
 };
